62266f0e9484b498cad90f87794e57c47538d8a0
bumbilo <dimagolyak@gmail.com>
2017-03-14 13:40:42 +0200

    step 2 added html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
-
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './src/app.js',
   output: {
-    filename: './dist/bundle.js'
-  }
+    path: 'dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My AppProject',
+      minify: {
+        collapseWhitespace: true,
+      },
+      hash: true,
+      template: './src/index.html'
+    })
+  ]
 }
