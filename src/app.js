f5489fda8923323e9927e908866d986ddd7140c7
bumbilo <dimagolyak@gmail.com>
2017-03-14 19:38:19 +0200

    add second page

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 //const css = require('./app.scss');
 import css from './app.scss';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('Hello from app.js webpack dev server')
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,38 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
+var webpack = require('webpack');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: './src/app.js',
+  entry: {
+    app: './src/app.js',
+    contact: './src/contact.js'
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
   module: {
     rules: [{
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: "style-loader",
-        use: ['css-loader', 'sass-loader']
-      })
-    }]
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
   },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
+    hot: true,
     stats: "errors-only",
-    open: true
-
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -32,8 +41,18 @@ module.exports = {
       //   collapseWhitespace: true,
       // },
       hash: true,
+      excludeChunks: ['contact'],
       template: './src/index.html'
     }),
-    new ExtractTextPlugin("app.css")
+    new HtmlWebpackPlugin({
+      title: 'My Contact',
+      hash: true,
+      chunks: ['contact'],
+      filename: 'contact.html',
+      template: './src/contact.html'
+    }),
+    new ExtractTextPlugin("app.css"),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin()
   ]
 }
