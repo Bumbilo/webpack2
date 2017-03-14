47fd941b221fe9b56f32f2003aae74b0278ef285
bumbilo <dimagolyak@gmail.com>
2017-03-14 18:35:27 +0200

    added webpack-dev-server options

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,14 @@ module.exports = {
       })
     }]
   },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    stats: "errors-only",
+    open: true
+
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'My AppProject',
